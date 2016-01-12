d7c57e87f5400109d274d47165ea8184ece825a4
jad617 <elasmar.jad@gmail.com>
2016-01-12 10:15:05 -0500

    fixing scripts

--- openstack_liberty/3.Nova/2.Compute/compute-step2.sh
+++ openstack_liberty/3.Nova/2.Compute/compute-step2.sh
@@ -14,13 +14,12 @@ cp sources/nova.conf /etc/nova/nova.conf
 
 chown nova.nova /etc/nova/nova.conf
 
-read -p "Quelle est l'IP du ce serveur?: " compute_ip
+read -p "What is the IP of your server?: " compute_ip
 
 sed -i "s/MY_IP/$compute_ip/g" /etc/nova/nova.conf
-###No need anymore since the MY_IP flag will change it too
-#sed -i "s/vncserver_proxyclient_address =/vncserver_proxyclient_address = $compute_ip/g" /etc/nova/nova.conf
 
 sed -i "s/NOVA_PASS/${nova_user_pass}/g" /etc/nova/nova.conf
+sed -i "s/RABBIT_PASS/${rabbit_pass}/g" /etc/nova/nova.conf
 
 service nova-compute restart
 
