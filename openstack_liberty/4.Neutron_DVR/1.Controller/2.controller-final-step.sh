cbfc4742709c6f53dc96d6a827479b704e78c925
jad617 <elasmar.jad@gmail.com>
2016-01-14 20:31:52 -0500

    almost finished neutron script

--- openstack_liberty/4.Neutron_DVR/1.Controller/2.controller-final-step.sh
+++ openstack_liberty/4.Neutron_DVR/1.Controller/2.controller-final-step.sh
@@ -4,8 +4,10 @@
 su -s /bin/sh -c "neutron-db-manage --config-file /etc/neutron/neutron.conf \
   --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head" neutron
 
+service openvswitch-switch restart
 service nova-api restart
 
+service openvswitch-switch restart
 service neutron-server restart
 service neutron-plugin-openvswitch-agent restart
 service neutron-dhcp-agent restart
@@ -15,18 +17,6 @@ service neutron-l3-agent restart
 
 rm -f /var/lib/neutron/neutron.sqlite
 
-#--------------------------------------------------------
-echo -e "\nWe now setup the external bridge : br-ex\n"
-
-service openvswitch-switch restart
-ovs-vsctl add-br br-ex
-
-ovs-vsctl add-port br-ex eth0
-
-service neutron-plugin-openvswitch-agent restart
-service neutron-l3-agent restart
-service neutron-dhcp-agent restart
-service neutron-metadata-agent restart
 
 echo -e "\nNeutron setup is done\n"
 read -p "We are now going to proceed with the verification. Pres any key to continue: " fake
